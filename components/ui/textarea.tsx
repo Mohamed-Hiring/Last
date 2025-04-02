19047624ba053e67a25332062bee52f543b47ee1
Mohamed-Hiring <m.khalid.skd@gmail.com>
2025-04-02 07:22:59 +0300

    Update textarea.tsx

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,10 +1,13 @@
+// textarea.tsx
 import { TextareaHTMLAttributes } from "react";
 
-export function Textarea(props: TextareaHTMLAttributes<HTMLTextAreaElement>) {
+const Textarea = (props: TextareaHTMLAttributes<HTMLTextAreaElement>) => {
   return (
     <textarea
       {...props}
       className="w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-purple-600"
     />
   );
-}
+};
+
+export default Textarea;
